231ce109550bbd2f78ca8ea5fe2fb87dee3dca43
Patrick <patrick.schwindt14@gmail.com>
2019-03-29 13:47:01 -0600

    Fixed again

--- data/routers/project-routers.js
+++ data/routers/project-routers.js
@@ -31,7 +31,7 @@ router.get('/:id', async (req, res) =>{
 router.post('/', async (req, res) =>{
     try{
         const project = await projectDB.insert(req.body);
-        res.status(201).json(action);
+        res.status(201).json(project);
     }catch(error){
         res.status(500).json({
             message: "Error adding project",
